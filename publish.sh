8928fa245a971d24f44790939207dd1d268b5de2
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-02-15 10:51:32 -0500

    Initial DOCS

--- publish.sh
+++ publish.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
+echo "NPM Publishing is disabled at this time"
 if npm audit; then
+  rm *orig* 
   npm run-script document
   npm run-script api
-  rm *orig* *toc\.*
   git add .
   git commit -m "$1"
-  npm version patch -m "$1"
-  npm publish
+ # npm version patch -m "$1"
+ # npm publish
   git commit -m "$1"
   git push origin master --tags
 else
